a839d1deb6ab3e99e8bde3b68bdbc1fd745f044b
tomaszjurek <tomaszoprzadek@gmail.com>
2018-01-14 18:34:43 +0100

    Added partials to scss, new subpage and modified bootstrap

--- js/app.jsx
+++ js/app.jsx
@@ -1,33 +1,29 @@
 $(function () {
 
   const nbpApi  = 'http://api.nbp.pl/api/exchangerates/tables/c/?format=json';
-  let tableInsert = $('table#currency').find('tbody');
 
   function insertCurrency(currency) {
-    for(let i = 0 ; i < currency.length; i++) {
-        let tr = $('<tr><tr>');
-        let code = $('<td>').text(currency[i].code);
-        let bid = $('<td>').text(currency[i].bid);
-        let ask = $('<td>').text(currency[i].ask);
+    let tableInsert = $('table#currency').find('tbody');
+    let dateInsert = $('table#currency').find('th').first();
+    dateInsert.text(currency.effectiveDate);
+    for(let i = 0 ; i < currency.rates.length; i++) {
+        let tr = $('<tr class="active"></tr>');
+        let code = $('<td>').text(currency.rates[i].code);
+        let bid = $('<td>').text(currency.rates[i].bid);
+        let ask = $('<td>').text(currency.rates[i].ask);
+        tableInsert.append(tr);
         tr.append(code);
         tr.append(bid);
         tr.append(ask);
-        tableInsert.append(tr)
     };
   }
-
-
-
-
-
-
 
   let loadCurrency = () => {
     $.ajax({
       url: nbpApi
     }).done(function(response){
       console.log(response[0]);
-      insertCurrency(response[0].rates)
+      insertCurrency(response[0])
     }).fail(function(error) {
        console.log(error);
     })
@@ -45,34 +41,3 @@ $(function () {
 
 
 });
-
-// var movieLists = $('.repertuar');
-// //variables for url
-// var movieUrl = 'https://swapi.co/api/films/';
-//
-//
-// /* Insert Movies to DOM  */
-// function insertContent(movies) {
-//   for(var i = 0 ; i < movies.length; i++) {
-//       var li = $('<li>', {class: "movie"});
-//       var h3 = $('<h3>').text(movies[i].title);
-//       li.append(h3);
-//       movieLists.append(li);
-//   };
-// }
-//
-// /* Load movies and insert them into the DOM
-// */
-// function loadMovies() {
-//       $.ajax({
-//           url: movieUrl
-//       }).done(function(response){
-//       insertContent(response.results);
-//      }).fail(function(error) {
-//          console.log(error);
-//      })
-// }
-//
-// loadMovies();
-//
-// });
