f8f9e5a9b52661df1131a53bbd9c46d6a7e48f83
tomaszjurek <tomaszoprzadek@gmail.com>
2018-01-15 19:44:26 +0100

    Added Calculator Form

--- js/app.jsx
+++ js/app.jsx
@@ -1,16 +1,16 @@
 $(function () {
 
-  const nbpApi  = 'http://api.nbp.pl/api/exchangerates/tables/c/?format=json';
+  const nbpApi  = "http://api.nbp.pl/api/exchangerates/tables/c/?format=json";
 
-  function insertCurrency(currency) {
-    let tableInsert = $('table#currency').find('tbody');
-    let dateInsert = $('table#currency').find('th').first();
+  let insertCurrency = (currency) => {
+    let tableInsert = $("table#currency").find("tbody");
+    let dateInsert = $("table#currency").find("th").first();
     dateInsert.text(currency.effectiveDate);
     for(let i = 0 ; i < currency.rates.length; i++) {
-        let tr = $('<tr class="active"></tr>');
-        let code = $('<td>').text(currency.rates[i].code);
-        let bid = $('<td>').text(currency.rates[i].bid);
-        let ask = $('<td>').text(currency.rates[i].ask);
+        let tr = $("<tr class='active'></tr>");
+        let code = $("<td>").text(currency.rates[i].code);
+        let bid = $("<td>").text(currency.rates[i].bid);
+        let ask = $("<td>").text(currency.rates[i].ask);
         tableInsert.append(tr);
         tr.append(code);
         tr.append(bid);
@@ -18,26 +18,46 @@ $(function () {
     };
   }
 
+  let insertExchange = (currency) => {
+    let currencyInsert = $("form#calculatorForm select#currencySelect");
+    for(let i = 0 ; i < currency.length; i++) {
+        let option = $("<option></option>").val(currency[i].code).text(currency[i].code).attr("data-id", [i]).attr("data-bid",currency[i].bid).attr("data-ask",currency[i].ask);
+        currencyInsert.append(option);
+    };
+  };
+
   let loadCurrency = () => {
     $.ajax({
       url: nbpApi
     }).done(function(response){
-      console.log(response[0]);
-      insertCurrency(response[0])
+      insertCurrency(response[0]);
+      insertExchange(response[0].rates);
     }).fail(function(error) {
        console.log(error);
     })
+
   };
   loadCurrency();
 
 
+    $("button.btn").on("click",	function(event)	{
+      event.preventDefault();
+      let optionSelected = $("select#currencySelect").find(":selected");
+      let checkboxValue = $('input[name=bidAsk]:checked', '#calculatorForm').val()
+      let calculator = $("div#calculator");
+      let choosenCurrency = $("div#calculator p").last();
 
-
-
-
-
-
-
-
-
+      if (checkboxValue === "bid") {
+        choosenCurrency.text("You have chosen" + " " + optionSelected.val() + " " + optionSelected.attr("data-bid"));
+        calculator.append(choosenCurrency);
+        console.log(optionSelected.attr("data-bid"));
+      } else if (checkboxValue === "ask") {
+        choosenCurrency.text("You have chosen" + " " + optionSelected.val() + " " + optionSelected.attr("data-ask"));
+        calculator.append(choosenCurrency);
+        console.log(optionSelected.attr("data-ask"));
+      } else if (checkboxValue === undefined) {
+        choosenCurrency.text("You need to choose bid or ask");
+        calculator.append(choosenCurrency);
+      }
+  });
 });
